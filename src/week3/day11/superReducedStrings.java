ac560559b683146113146a1f8e9da4bebcea0fac
Prayas Jain <p.jain3322@gmail.com>
2019-07-27 21:04:49 +0530

    complete Day 11 problem 1.

--- src/week3/day11/superReducedStrings.java
+++ src/week3/day11/superReducedStrings.java
@@ -1,4 +1,33 @@
 package week3.day11;
 
+import java.util.Stack;
+
 public class superReducedStrings {
+    public static void main(String[] args) {
+        System.out.println(findReducedString("abba"));
+        System.out.println(findReducedString("aaabccddd"));
+    }
+
+    private static String findReducedString(String input) {
+
+
+        StringBuilder output = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++) {
+            if (stack.isEmpty()) {
+                stack.push(input.charAt(i));
+            } else {
+                if (stack.peek() == input.charAt(i)) {
+                    stack.pop();
+                } else {
+                    stack.push(input.charAt(i));
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            output.append(stack.pop());
+        }
+        return output.length() == 0 ? "Empty String" : output.reverse().toString();
+    }
 }
