fe2162ad2717666ab2e4ba8e3330f86f314a83f6
Prayas Jain <p.jain3322@gmail.com>
2019-08-03 14:49:51 +0530

    complete day 15 problem 3.

--- src/week4/day15/SingleNumber.java
+++ src/week4/day15/SingleNumber.java
@@ -1,4 +1,16 @@
 package week4.day15;
 
 public class SingleNumber {
+    public static void main(String[] args) {
+        System.out.println(singleNumber(new int[]{2, 2, 1}));
+        System.out.println(singleNumber(new int[]{4, 1, 2, 1, 2}));
+    }
+
+    private static int singleNumber(int[] nums) {
+        int output = 0;
+        for (int i = 0; i < nums.length; i++) {
+            output = output ^ nums[i ];
+        }
+        return output;
+    }
 }
